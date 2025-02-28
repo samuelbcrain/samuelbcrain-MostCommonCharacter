c14c037f57ba84cd4997ad2f578cce36b28d6987
Samuel Crain <samuelbcrain@gmail.com>
2025-02-28 04:14:20 +0000

    Coded method to get most common char in String.

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +9,31 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        HashMap<Character, Integer> charCount = new HashMap<Character, Integer>();
+        char charToAdd = ' ', result = ' ';
+
+        // for all chars in string
+        for (int i = 0; i < str.length(); i++) {
+            charToAdd = str.charAt(i);
+            // if char NOT in hashmap, add to hashmap
+            if (!charCount.containsKey(charToAdd)) {
+                charCount.put(charToAdd, 1);
+            }
+            // if char IS in hashmap, increase count
+            else {
+                charCount.put(charToAdd, charCount.get(charToAdd) + 1);
+            }
+        }
+
+        result = str.charAt(0);
+        // for all chars in hashmap
+        for (char c : charCount.keySet()) {
+            // if larger count found than current result, set new result
+            if (charCount.get(c) > charCount.get(result)) {
+                result = c;
+            }
+        }
+
+        return result;
     }
 }
